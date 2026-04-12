990f0c1eea5047ab802291f8e904cc91e9c2f1bd
uries242 <teykoa@gmail.com>
2026-04-11 23:31:51 -0400

    Updated API calls

--- src/models/api.js
+++ src/models/api.js
@@ -1,1 +1,20 @@
-// fetch logic 
+// fetch logic
+
+
+var apiKey = "at_jSpNDc9OnlncMeCSGQN9AWEmulrNX";
+
+async function getIpData(ipAddress) {
+  try {
+    const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`);
+    const data = await response.json();
+    return data;
+  } 
+  catch (error) {
+    console.log("Unable to retrieve data", error);
+  }
+}
+
+
+//  CALL FUNCTION
+// getIpData("209.85.231.104") 
+// .then(data => console.log(data));
